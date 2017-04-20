6b8d06def8994a32c5b331a7345102eeddb642af
JeffDev74 <jeffdev74@gmail.com>
2017-04-20 01:57:18 -0700

    started working on the enemy

--- Assets/Code/Health.cs
+++ Assets/Code/Health.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Networking;
 
 namespace FPS
 {
-	public class Health : MonoBehaviour
+	public class Health : NetworkBehaviour
 	{
+        private Transform _theTransform;
+        private Transform TheTransform
+        {
+            get
+            {
+                if(_theTransform == null)
+                {
+                    _theTransform = transform;
+                }
+                return _theTransform;
+            }
+        }
+
         public const int maxHealth = 100;
+
+        [SyncVar(hook = "OnChangeHealth")]
         public int currentHealth = maxHealth;
+
+        void OnChangeHealth(int health)
+        {
+            currentHealth = health;
+            healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+        }
 
         public RectTransform healthBar;
 
@@ -15,11 +36,20 @@ namespace FPS
             currentHealth -= amount;
             if(currentHealth <= 0)
             {
-                currentHealth = 0;
-                Debug.Log("Dead!");
-            }
+                currentHealth = maxHealth;
 
-            healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+                // called on the server but invoked on the clients
+                RpcRespawn();
+            }
+        }
+
+        [ClientRpc]
+        void RpcRespawn()
+        {
+            if(isLocalPlayer)
+            {
+                TheTransform.position = Vector3.zero;
+            }
         }
 	}
 }
