2f986bbb7f012c1d49582007166ef4bbb026f0a6
JeffDev74 <jeffdev74@gmail.com>
2017-04-20 02:19:07 -0700

    working on the enemy

--- Assets/Code/EnemySpawner.cs
+++ Assets/Code/EnemySpawner.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
 namespace FPS
 {
-	public class EnemySpawner : MonoBehaviour
+	public class EnemySpawner : NetworkBehaviour
 	{
+        public GameObject enemyPrefab;
+        public int numberOfEnemies;
 
-	}
+        public override void OnStartServer()
+        {
+            for (int i = 0; i < numberOfEnemies; i++)
+            {
+                var spawnPosition = new Vector3(Random.Range(-8, 8), 0.0f, Random.Range(-8, 8));
+
+                var spawnRotation = Quaternion.Euler(0.0f, Random.Range(0, 180), 0.0f);
+
+                var enemy = Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+                NetworkServer.Spawn(enemy);
+            }
+        }
+    }
 }
