4c17244106d941c5c1b0aed63d83651f87d3dfc3
JeffDev74 <jeffdev74@gmail.com>
2017-04-19 22:21:35 -0700

    working on player movement

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace FPS
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Transform _theTransform;
+        private Transform TheTransform
+        {
+            get
+            {
+                if (_theTransform == null)
+                {
+                    _theTransform = transform;
+                }
+                return _theTransform;
+            }
+        }
+
+        private void Update()
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            TheTransform.Rotate(0, x, 0);
+            TheTransform.Translate(0, 0, z);
+        }
+    }
+}
