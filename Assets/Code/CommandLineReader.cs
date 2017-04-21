e631d4412fa63d1859d4db14ab6c9661e569fa01
Kyle Olsen <kyledouglasolsen@gmail.com>
2017-04-21 00:47:17 -0700

    ip points to kdo ec2 server added CommandLineReader to handle headless server starting

---
+++ Assets/Code/CommandLineReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CommandLineReader : MonoBehaviour
+{
+    private void Start()
+    {
+        var headless = false;
+        var allArgs = Environment.GetCommandLineArgs();
+
+        for (var i = 0; i < allArgs.Length; ++i)
+        {
+            var arg = allArgs[i];
+
+            if (arg == "-batchmode" || arg == "-nographics")
+            {
+                headless = true;
+            }
+        }
+
+        if (headless)
+        {
+            NetworkManager.singleton.StartServer();
+        }
+    }
+}
